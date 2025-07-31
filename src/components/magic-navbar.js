4c27ec9ad824f8e78a4d2a3b65b0721fd7fda1ae
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2025-07-31 12:11:38 -0400

    reset up

--- src/components/magic-navbar.js
+++ src/components/magic-navbar.js
@@ -150,7 +150,7 @@ export default function MagicNavbar() {
                 <div className="h-10 w-10 relative">
                   <Image
                     src="/LogoFinal/ONLY LOGO svgs/Only_Logo_High.svg"
-                    alt="Fermi Energy"
+                    alt="Fermi Energy Logo"
                     width={40}
                     height={40}
                     className="h-full w-full object-contain"
