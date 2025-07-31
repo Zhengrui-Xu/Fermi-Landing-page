c6f5781e9c4445962c995eba3d2f5c3129096f39
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2025-07-31 11:42:43 -0400

    ready

--- src/components/magic-navbar.js
+++ src/components/magic-navbar.js
@@ -92,7 +92,7 @@ export default function MagicNavbar() {
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
           <svg
-            strokeWidth="1.5"
+            strokeWidth="1.4"
             viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
