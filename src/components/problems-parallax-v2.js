feb490f4091f35afc8ad87934e303f3239291fe2
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2025-08-23 21:37:19 -0400

    removed scroll

--- src/components/problems-parallax-v2.js
+++ src/components/problems-parallax-v2.js
@@ -397,16 +397,6 @@ const ProblemsParallaxV2 = () => {
             </div>
           ))}
         </div>
-
-        {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-gray-600 z-40">
-          <div className="flex flex-col items-center">
-            <span className="text-sm mb-2">Scroll to explore</span>
-            <div className="w-6 h-10 border-2 border-gray-400 rounded-full flex justify-center">
-              <div className="w-1 h-3 bg-gray-500 rounded-full mt-2 animate-bounce" />
-            </div>
-          </div>
-        </div>
       </section>
     </>
   )
